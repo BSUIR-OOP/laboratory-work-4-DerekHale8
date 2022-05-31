001f60f3fde500344a3ede45459a81145097c410
DerekHale8 <shurik18110@gmail.com>
2022-05-31 10:42:22 +0300

    Add Library Files

---
+++ DILib/DIContainer.cs
@@ -0,0 +1,29 @@
+﻿namespace DILib
+{
+    public class DIContainer
+    {
+        private List<ServiceDescriptor> descriptors;
+        public DIContainer(List<ServiceDescriptor> descriptors)
+        {
+            this.descriptors = descriptors;
+        }
+
+        public T GetServiceType<T>()
+            => (T)GetServiceType(typeof(T));
+
+        private object GetServiceType(Type serviceType)
+        {
+            var service = descriptors.SingleOrDefault(x => x.ServiceType == serviceType);
+            var descriptor = descriptors.SingleOrDefault(x => x.ServiceType == serviceType);
+            if (descriptor.Implementation != null)
+                return descriptor.Implementation;
+
+            var actType = descriptor.ImplementationType == null ? descriptor.ServiceType : descriptor.ImplementationType;
+            var constructorInfo = actType.GetConstructors()[0];
+            object[] parameters = constructorInfo.GetParameters().Select(x => GetServiceType(x.ParameterType)).ToArray();
+            var implementation = Activator.CreateInstance(actType, parameters);
+            descriptor.Implementation = implementation;
+            return implementation;
+        }
+    }
+}
---
+++ DILib/ServiceDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DILib
+{
+    public class ServiceDescriptor
+    {
+        public Type ServiceType { get; set; }
+        public Type ImplementationType { get; set; }
+        public object Implementation { get; set; }
+        public ServiceTime Lifetime { get; set; }
+
+        public ServiceDescriptor(Type serviceType, Type implementationType, ServiceTime lifetime)
+        {
+            ServiceType = serviceType;
+            ImplementationType = implementationType;
+            Lifetime = lifetime;
+        }
+    }
+}
